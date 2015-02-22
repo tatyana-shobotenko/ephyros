30b4c1a13512316adbf5ed944fd31c5d0aa384fd
Bogdan Savluk <savluk.bogdan@gmail.com>
2015-02-22 04:51:29 +0200

    simple webpack gulp integration

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,46 @@
 'use strict';
 
 var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
+var rubySass = require('gulp-ruby-sass');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+
+var gutil = require("gulp-util");
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
 
 var webPath = 'public';
 var srcPath = 'src';
 
 gulp.task('styles', function () {
-  return $.rubySass(srcPath + '/styles', {
+  return rubySass(srcPath + '/styles', {
     sourcemap: true,
     compass: true
   })
     .on('error', function (err) {
       console.error('Error!', err.message);
     })
-    .pipe($.postcss([
+    .pipe(postcss([
       // more here https://github.com/postcss/postcss
       require('autoprefixer-core')({browsers: ['last 1 version']})
     ]))
-    .pipe($.sourcemaps.write('.'))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(webPath + '/styles'));
 });
 
-gulp.task('watch', ['styles'], function () {
+gulp.task("webpack", function (callback) {
+  // run webpack
+  webpack(require('./webpack.config'),
+    function (err, stats) {
+      if (err) throw new gutil.PluginError("webpack", err);
+      gutil.log("[webpack]", stats.toString({
+        // output options
+      }));
+      callback();
+    });
+});
 
+gulp.task('watch', ['styles', 'webpack'], function () {
   gulp.watch(srcPath + '/styles/**/*', ['styles']);
+  gulp.watch(srcPath + '/**/*.js', ['webpack']);
 });
