cfa5f038c9ff5a76def9dc6c30186c4e504c9736
Bogdan Savluk <savluk.bogdan@gmail.com>
2015-02-26 09:46:07 +0200

    build styles using libsass

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,18 @@
 'use strict';
 
 var gulp = require('gulp');
-var rubySass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
+var gulpIf = require('gulp-if');
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 
 var srcPath = 'src';
 
 gulp.task('styles', function () {
-  return rubySass(srcPath + '/styles', {
-   // sourcemap: true,
-    compass: true
-  })
-    .on('error', function (err) {
-      console.error('Error!', err.message);
-    })
+  return gulp.src(srcPath + '/styles/*.{scss,sass}')
+    .pipe(sourcemaps.init())
+    .pipe(gulpIf(/\.sass$/, sass({indentedSyntax: true})))
+    .pipe(gulpIf(/\.scss$/, sass()))
     .pipe(postcss([
       // more here https://github.com/postcss/postcss
       require('autoprefixer-core')({browsers: ['last 1 version']})
