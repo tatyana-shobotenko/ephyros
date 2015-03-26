249f2d6aafd6758b93410f761a00c57c9a8506d0
Tetiana Lyko <avoska.bljalja@gmail.com>
2015-03-26 18:43:18 +0200

    new data update: added team photos, and 2 new team members

--- src/data/team/photoSlides.js
+++ src/data/team/photoSlides.js
@@ -10,9 +10,39 @@ var slides = [
     shadowModifier: ''
   },
   {
-    image: require('./slides/katiaOH.jpg'),
-    title: 'Катя is near offline stand belonging to one of her projects',
-    shadowModifier: 'photo-screen__shadow_violet'
+    image: require('./slides/girls.jpg'),
+    title: 'Girls, uh girls',
+    shadowModifier: ''
+  },
+  {
+    image: require('./slides/katia_onphone.jpg'),
+    title: 'Kate having very productive meeting with lovely client',
+    shadowModifier: ''
+  },
+  {
+    image: require('./slides/vetal.jpg'),
+    title: 'Peace, bro!',
+    shadowModifier: ''
+  },
+  {
+    image: require('./slides/katia_sasha.jpg'),
+    title: 'It\'s always sunny in our office',
+    shadowModifier: ''
+  },
+  {
+    image: require('./slides/pedobear.jpg'),
+    title: 'Team\'s amulet =)',
+    shadowModifier: ''
+  },
+  {
+    image: require('./slides/brain.jpg'),
+    title: 'Don\'t forget to ...',
+    shadowModifier: ''
+  },
+  {
+    image: require('./slides/board.jpg'),
+    title: 'Place of inspiration',
+    shadowModifier: ''
   }
 ];
 module.exports = slides;
