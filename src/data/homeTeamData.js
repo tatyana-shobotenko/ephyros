b5e0a802d6e7d0d8c014a731ac58044f21c4a12f
Tetiana Lyko <avoska.bljalja@gmail.com>
2015-04-08 19:12:48 +0300

    store team avas in project, tiny photo optimization

--- src/data/homeTeamData.js
+++ src/data/homeTeamData.js
@@ -1,6 +1,6 @@
-var casesData = [
+var hTeamData = [
   {
-    photo: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xpa1/v/t1.0-1/c59.5.842.842/s160x160/10351961_686642578084523_8185756175983947040_n.jpg?oh=375bf3f7df20c7b6f5332d97d497d906&oe=55876044&__gda__=1434610435_ac5799d34f0709a0acf30d4fdf1c7f40",
+    photo: require("./team/ava/pasha.png"),
     name: "Pavel",
     position: "Managing Director"
   },
@@ -10,7 +10,7 @@ var casesData = [
     position: "Technical Lead"
   },
   {
-    photo: "https://pp.vk.me/c403822/v403822911/bc9c/cRWDciE413w.jpg",
+    photo: require("./team/ava/takka.jpg"),
     name: "Tania",
     position: "Frontend Lead"
   },
@@ -20,12 +20,12 @@ var casesData = [
     position: "Backend Developer"
   },
   {
-    photo: "https://pp.vk.me/c620028/v620028895/15307/17amHd6yRTQ.jpg",
+    photo: require("./team/ava/hanna.jpg"),
     name: "Ania",
     position: "Frontend Developer"
   },
   {
-    photo: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xpf1/v/t1.0-1/c0.0.160.160/p160x160/10710847_503387579764891_7436691124456846245_n.jpg?oh=61bde0050e4ef4923aff0fe4784bf3ba&oe=552BD22E&__gda__=1428231420_2c5613a606d4403023e0893ab363906f",
+    photo: require("./team/ava/kate.jpg"),
     name: "Katia",
     position: "Project Manager"
   },
@@ -35,15 +35,15 @@ var casesData = [
     position: "Backend Developer"
   },
   {
-    photo: "https://lh3.googleusercontent.com/-ixKsk2H0rr8/AAAAAAAAAAI/AAAAAAAAAAs/71ThRZH_9dA/s120-c/photo.jpg",
+    photo: require("./team/ava/vica.jpg"),
     name: "Victoria",
     position: "Frontend Developer"
   },
   {
-    photo: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfp1/v/t1.0-1/p160x160/1239488_528687010544885_28541602_n.jpg?oh=2db660c116d13fee223c12e8245fda45&oe=5582EC67&__gda__=1434896311_9a69e3305f99f42f27744b2f8301501e",
+    photo: require("./team/ava/vania.jpg") ,
     name: "Ivan",
     position: "QA Engineer"
   }
 ];
 
-module.exports = casesData;
+module.exports = hTeamData;
