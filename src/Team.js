f8f0c603f1397bb11c4c0eef03f42ed313225752
Bogdan Savluk <savluk.bogdan@gmail.com>
2015-10-27 03:38:43 +0200

    fix import name

--- src/Team.js
+++ src/Team.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Link} from 'react-router';
 
 import BottomMenu from './BottomMenu';
-import TeamSlides from './TeamSlides';
+import Slides from './TeamSlides';
 
 
 import teamData from './data/teamData';
