27e965640c456377972ce40e57ca2df584960fd8
Bogdan Savluk <savluk.bogdan@gmail.com>
2015-11-01 03:36:31 +0200

    move ga to separate file

---
+++ src/client/ga.js
@@ -0,0 +1,17 @@
+(function (i, s, o, g, r, a, m) {
+  i.GoogleAnalyticsObject = r;
+  i[r] = i[r] || function () {
+      (i[r].q = i[r].q || []).push(arguments);
+    };
+  i[r].l = Date.now();
+  a = s.createElement(o);
+  m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+window.ga('create', 'UA-40296016-1', 'auto');
+
+window.gae = function (eventCategory, eventAction, eventLabel, eventValue, fieldsObject) {
+  window.ga('send', 'event', eventCategory, eventAction, eventLabel, eventValue, fieldsObject);
+};
