8cc05da9ba8bf60e922516f9650412788489d6f3
Tanya Lyko <avoska.bljalja@gmail.com>
2018-01-13 17:16:18 +0200

    intro screen

--- src/homePage/Intro.js
+++ src/homePage/Intro.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'router1-react';
 
+import MVPIcon from '../data/services/MVP.png';
+import SAASIcon from '../data/services/SAAS.png';
+import ICOIcon from '../data/services/ICO.png';
+
 class Intro extends Component {
   constructor() {
     super();
@@ -46,28 +50,43 @@ class Intro extends Component {
           <div className="center-wrapper">
             <div className="screen-intro__content">
               <h1 className="screen-intro__title">
-                We create rocking web applications for startups &amp;
-                enterprises
+                One Stop Shop Solution for your ideas
               </h1>
 
               <p className="screen-intro__subtitle">
-                At Ephyros we help ambitious clients to bring powerful ideas to
-                life
+                Focus on your business, we will do the rest
               </p>
 
-              <div className="button-group">
-                <Link route="cases" className="button button-group__item">
-                  Selected Cases
-                </Link>
-                <Link
-                  route="services"
-                  className="button button-group__item"
-                  onClick={() => {
-                    window.gae('mainpage', 'click', 'services', 25);
-                  }}
-                >
-                  Services
-                </Link>
+              <div className="intro-services">
+                <div className="intro-service">
+                  <img src={MVPIcon} alt="MVP" />
+                  <p className="intro-service__title">MVP development</p>
+                  <p>Idea validation, Tracing, getting first users</p>
+                </div>
+                <div className="intro-services__dots">
+                  <div className="intro-services__dot" />
+                  <div className="intro-services__dot" />
+                  <div className="intro-services__dot" />
+                  <div className="intro-services__dot" />
+                  <div className="intro-services__dot" />
+                </div>
+                <div className="intro-service">
+                  <img src={SAASIcon} alt="MVP" />
+                  <p className="intro-service__title">SaaS infrustructure</p>
+                  <p>Operational exellence</p>
+                </div>
+                <div className="intro-services__dots">
+                  <div className="intro-services__dot" />
+                  <div className="intro-services__dot" />
+                  <div className="intro-services__dot" />
+                  <div className="intro-services__dot" />
+                  <div className="intro-services__dot" />
+                </div>
+                <div className="intro-service">
+                  <img src={ICOIcon} alt="MVP" />
+                  <p className="intro-service__title">ICO</p>
+                  <p>For mature businesses</p>
+                </div>
               </div>
             </div>
           </div>
